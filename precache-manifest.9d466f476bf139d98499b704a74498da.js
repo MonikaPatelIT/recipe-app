b81a5618e4ff1f80f99d6347ffff2c7ff0ac0c57
Monika Patel <monikapatel.it@outlook.com>
2019-02-22 10:43:53 +1300

    Updates

---
+++ precache-manifest.9d466f476bf139d98499b704a74498da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/recipe-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e8600937274dafcc5ad5",
+    "url": "/recipe-app/static/css/main.ee370247.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/recipe-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e9004d8d5cd3b5289ba5",
+    "url": "/recipe-app/static/js/1.e9004d8d.chunk.js"
+  },
+  {
+    "revision": "71cff4495fe45cdae67d",
+    "url": "/recipe-app/static/js/runtime~main.71cff449.js"
+  },
+  {
+    "revision": "e8600937274dafcc5ad5",
+    "url": "/recipe-app/static/js/main.e8600937.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/recipe-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/recipe-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/recipe-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "e9004d8d5cd3b5289ba5",
+    "url": "/recipe-app/static/css/1.e394894e.chunk.css"
+  },
+  {
+    "revision": "a774676a5705068af7f18e98a52ddbb8",
+    "url": "/recipe-app/index.html"
+  }
+];
